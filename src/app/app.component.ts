a5ca81cb0b245303cc0b9ee304923f09aeda56bd
bailooon <matheus@bailon.com.br>
2025-04-09 19:53:37 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InterpolacaoComponent } from './components/interpolacao/interpolacao.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InterpolacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/interpolacao/interpolacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './interpolacao.component.html',
+  styleUrl: './interpolacao.component.css'
+})
+export class InterpolacaoComponent {
+
+}
