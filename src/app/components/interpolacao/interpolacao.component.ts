4578838df9451ab0e399a1d7bb0342fc013066ef
bailooon <matheus@bailon.com.br>
2025-04-09 20:21:23 -0300

    aprendendo usar a Interpolação

--- src/app/components/interpolacao/interpolacao.component.ts
+++ src/app/components/interpolacao/interpolacao.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-interpolacao',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './interpolacao.component.html',
   styleUrl: './interpolacao.component.css'
 })
 export class InterpolacaoComponent {
+  name: string = 'Matheus';
+  age: number = 19;
+  job = 'Developer';
+  hobbies = ['Music', 'Sports', 'Movies'];
+  car = {make: 'Ford', model: 'Fiesta'};
+  img = '../../../assets/pexels-mickhaupt-5668029 (1).jpg';
 
 }
