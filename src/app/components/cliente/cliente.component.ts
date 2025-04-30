648f1c2fc50d2953937b28494303b909d57b5487
bailooon <matheus@bailon.com.br>
2025-04-30 20:14:57 -0300

    adicionando funcionalidade no método save

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import{Cliente} from '../../interfaces/Clientes';
+import { Cliente } from '../../interfaces/Clientes';
 import { ClienteService } from '../../services/cliente.service';
 import { ReactiveFormsModule, Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -13,29 +13,52 @@ import { CommonModule } from '@angular/common';
   styleUrl: './cliente.component.css'
 })
 export class ClienteComponent {
-  clienteForm : FormGroup = new FormGroup({})
+  clienteForm: FormGroup = new FormGroup({})
   cliente: Cliente[] = []
 
-  constructor(private clienteService:ClienteService, private formBuilder:FormBuilder){
+  constructor(private clienteService: ClienteService, private formBuilder: FormBuilder) {
     this.clienteForm = formBuilder.group({
-      nome:['', Validators.required],
+      nome: ['', Validators.required],
       telefone: ['']
     });
 
   }
 
-  list(): void{
+  list(): void {
     this.cliente = this.clienteService.list()
   }
 
   //método executado ao inicializar a página
-  ngOnInit():void{
+  ngOnInit(): void {
     this.list()
   }
 
-  save(){
-    if(this.clienteForm.valid){
-      alert('salvando!')
+
+  generateRandomString(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  save() {
+    if (this.clienteForm.valid) {
+      const formData = this.clienteForm.value
+      const clienteAdd: Cliente = {
+        id: this.generateRandomString(6),
+        nome: formData.nome,
+        telefone: formData.telefone
+      }
+      // console.log(clienteAdd)
+      this.clienteService.add(clienteAdd) //Chamando a service para inserir
+      alert('Inserido com sucesso') // Enviando feedback ao usuário
+      this.clienteForm.reset // Limpar o form após o preenchimento
+      this.list()
+    } else {
+      alert('Por favor preencher os campos obrigatórios')
     }
   }
 
