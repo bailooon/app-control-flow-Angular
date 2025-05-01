ad8db209b0eead744b8f72ffd63760821146f245
bailooon <matheus@bailon.com.br>
2025-04-30 21:58:06 -0300

    Criando Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { PropertieBindingComponent } from './components/propertie-binding/proper
 import { DiretivaComponent } from './components/diretiva/diretiva.component';
 import {Carros} from './interfaces/Carros';
 import { ClienteComponent } from './components/cliente/cliente.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InterpolacaoComponent, ControlFlowComponent, PropertieBindingComponent, DiretivaComponent, ClienteComponent],
+  imports: [RouterOutlet, InterpolacaoComponent, ControlFlowComponent, PropertieBindingComponent, DiretivaComponent, ClienteComponent, HomeComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { ControlFlowComponent } from './components/control-flow/control-flow.component';
+import path from 'path';
+import { Component } from '@angular/core';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'cliente', component:ClienteComponent},
+    {path: 'control-flow', component:ControlFlowComponent},
+    {path: '**', component:NotfoundComponent}
+];
